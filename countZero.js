a6de3eb53cfe1c3805e6dced70a500c0e1a9fdf4
EricRiney <fails.to.install@gmail.com>
2017-05-26 11:47:10 -0700

    changed

--- countZero.js
+++ countZero.js
@@ -2,12 +2,12 @@
 
 function countZero(n){
   var count = 0;
-  while(n>0){
+  while(n > 0){
    count += Math.floor(n/10);
    n = n/10;
   }
   return count;
 }
 
-// > countZero(2014);
+ console.log(countZero(2014));
 //   = 223
