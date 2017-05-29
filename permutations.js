c19cbf2a297b24379df212832bf8a82bcef9cb41
EricRiney <fails.to.install@gmail.com>
2017-05-29 11:07:16 -0700

    minor change to permutations.js

--- permutations.js
+++ permutations.js
@@ -12,14 +12,14 @@ var arr = str.split(''),
     if (len == 0)
         return [str];
 
-    for (var i=0; i<len; i++)
+    for (var i = 0; i < len; i++)
     {
         rest = Object.create(arr);
         picked = rest.splice(i, 1);
 
         restPerms = permutations(rest.join(''));
 
-       for (var j=0, jLen = restPerms.length; j< jLen; j++)
+       for (var j = 0, jLen = restPerms.length; j < jLen; j++)
        {
            next = picked.concat(restPerms[j]);
            perms.push(next.join(''));
