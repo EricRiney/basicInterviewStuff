8734a9a90d9d921a7de43f7bbf9af170a008b20f
EricRiney <fails.to.install@gmail.com>
2017-06-07 17:10:41 -0700

    good to go

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,11 +1,11 @@
 // removes the duplicates from an array 
 
 function removeDuplicate(arr) {
-  var exists ={},
-      outArr = [],
-      elm;
+  var exists = {};
+  var outArr = [];
+  var elm;
 
-  for(var i =0; i<arr.length; i++) {
+  for(var i = 0; i < arr.length; i++) {
     elm = arr[i];
     if(!exists[elm]){
       outArr.push(elm);
@@ -15,5 +15,5 @@ function removeDuplicate(arr) {
   return outArr;
 }
 
-// > removeDuplicate([1,3,3,3,1,5,6,7,8,1]);
+var test =  removeDuplicate([1,3,3,3,1,5,6,7,8,1]);
 //   = [1, 3, 5, 6, 7, 8]
