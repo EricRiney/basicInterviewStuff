d522bde1d78b04c56a80007d23082e527279d3c2
EricRiney <fails.to.install@gmail.com>
2017-07-07 19:55:00 -0700

    whatWill03.js

---
+++ whatWill03.js
@@ -0,0 +1,13 @@
+// What will be the output of the following code?
+
+var x = 1;
+var output = (function(){
+    delete x;
+    return x;
+  })();
+  
+  console.log(output);
+
+  // The output would be 1. 
+  // The delete operator is used to delete the property of an object. 
+  // Here x is not an object, but rather it's the global variable of type number.
