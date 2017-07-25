5a44fb28964537962334a621a61fa2085842f1ab
EricRiney <fails.to.install@gmail.com>
2017-07-25 10:30:12 -0700

    fixed typo

--- topSum.js
+++ topSum.js
@@ -2,27 +2,25 @@
 
 function topSum(arr) {
   var biggest = arr[0],
-      second = arr[1],
-      len = arr.length,
-      i = 2;
+  second = arr[1],
+  len = arr.length;
 
   if (len<2) return null;
-
+  
   if (biggest<second){
     biggest = arr[1];
     second = arr[0];
   }
 
-  for(; i<len; i++) {
-
-   if(arr[i] > biggest) {
+  for(var i = 2; i < len; i++) {
+    
+    if(arr[i] > biggest) {
       second = biggest;
       biggest = arr[i];
     }
-   else if (arr[i]>second) {
+    else if (arr[i]>second) {
       second = arr[i];
-   }
-
- }
+    }
+  }
  return biggest + second;
 }
