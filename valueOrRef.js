04d9613c6d8b38f9792c518833b079c4beaccc9c
EricRiney <fails.to.install@gmail.com>
2017-07-25 10:25:55 -0700

    fixed typo

--- valueOrRef.js
+++ valueOrRef.js
@@ -27,5 +27,5 @@ change(num, name, obj1, obj2);
 console.log(num); // 10
 console.log(name);// "Addy Osmani"
 console.log(obj1.value);//"first value"
-console.log(obj2.valuee);//"new value"
-console.log(obj3.valuee);//"new value"  
+console.log(obj2.value);//"new value"
+console.log(obj3.value);//"new value"  
