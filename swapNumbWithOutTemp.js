bddf7a6ff343afbc13f355046a60341d328c28a4
EricRiney <fails.to.install@gmail.com>
2017-08-18 17:25:39 -0700

    blah

--- swapNumbWithOutTemp.js
+++ swapNumbWithOutTemp.js
@@ -11,3 +11,16 @@ function swapNumb(a, b) {
 // > swapNumb(2, 3);
 //    = before swap:  a:  2 b:  3
 //    = after swap:  a:  3 b:  2
+
+function MergeStrings(strings) {
+    var s;
+    var len = strings.length();
+    for(var i = 0; i <= len; i++){
+        console.log("hello");
+        s += strings[i];
+    }
+    s.sort();
+    return s;
+}
+
+str.match(/[a-z]/)
